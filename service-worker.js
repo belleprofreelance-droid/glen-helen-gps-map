1026230db6ea3972d15d9a386a7f7f8caf8e0267
belleprofreelance-droid <belleprofreelance@gmail.com>
2026-05-15 17:56:07 -0400

    Cache map image for offline field use

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'glen-helen-gps-map-v0-1-0';
+const CACHE_NAME = 'glen-helen-gps-map-v0-1-1';
 const ASSETS = [
   './',
   './index.html',
@@ -6,17 +6,37 @@ const ASSETS = [
   './app.js',
   './manifest.webmanifest',
   './data/map-config.json',
-  './data/calibration-points.json'
+  './data/calibration-points.json',
+  './assets/map/glen-helen-map-web.jpg'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const fetchAndCache = fetch(event.request).then(response => {
+        if (response && response.status === 200 && response.type !== 'opaque') {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+        }
+        return response;
+      });
+
+      return cached || fetchAndCache;
+    }).catch(() => caches.match('./index.html'))
+  );
 });
